e7497eaa0d3e8ce3a5fdfc7dd370a82ce71272c6
COZAX <sushibar2929@gmail.com>
2022-05-17 20:54:09 +0200

    Navbar added | Working on the main page

---
+++ components/NavbarSP.vue
@@ -0,0 +1,16 @@
+<template>
+  <section id='navbar'>
+    <ul>
+      <li><img src="icon.svg"></li>
+    </ul>
+
+    <ul>
+      <li><button>Connexion</button></li>
+    </ul>
+  </section>
+</template>
+<script>
+export default {
+  name: 'NavbarSP'
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,7 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'speedpratik',
+    title: 'Speedpratik',
     htmlAttrs: {
       lang: 'en'
     },
@@ -12,12 +12,13 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/svg+xml', href: 'icon.svg' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '@/assets/css/main.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
