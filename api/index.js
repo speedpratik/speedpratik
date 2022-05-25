dba42270ca4bfcb8688e996c2b6778781c72c561
Romain Seznec <contact@romainseznec.com>
2022-05-25 21:30:42 +0200

    Temporary console.log to test docker image

--- api/index.js
+++ api/index.js
@@ -14,6 +14,8 @@ db.initialize()
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+console.log(process.env.DISCORD_CLIENT_ID, process.env.API_AUTHORIZATION_TOKEN)
+
 app.use((req, res, next) => {
   switch (req.get('authorization_token')) {
     case undefined:
